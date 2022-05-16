960b65f36c50717a0fd09ded940abcd34b037c85
Szymcio1225 <39444666+Szymcio1225@users.noreply.github.com>
2022-05-16 19:57:56 +0200

    Update quicklanguagechanger.sh

--- quicklanguagechanger.sh
+++ quicklanguagechanger.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
 
-var_a="en_US.UTF-8"
-var_b="pl_PL.UTF-8"
-
-echo "Default interface language is set to '$var_a' and region is set to '$var_b'"
 echo "Please type interface language and region"
 echo ""
 
 echo "Set interface language:"
 read $var_a
-clear
-echo "Interface language set to: $var_a"
-
-echo ""
-echo "Set region:"
-read $var_b
-clear
-echo "Region set to: $var_b"
 
 echo "LANG=$var_a
-LANGUAGE=$var_a
-LC_NUMERIC=$var_b
+LANGUAGE=$var_a" | tee -a /etc/locale.conf /etc/default/locale
+
+clear
+
+echo "Set region:"
+read $var_b
+
+echo "LC_NUMERIC=$var_b
 LC_TIME=$var_b
 LC_COLLATE=$var_b
 LC_MONETARY=$var_b
